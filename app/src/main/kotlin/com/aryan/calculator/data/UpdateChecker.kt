fab6e87773ac41d948c1ef952eca0d5e2ce304e3
chaturvediaryan2024 <chaturvediaryan2024@gmail.com>
2026-07-22 00:27:38 +0530

    Update version to 3.2

--- app/src/main/kotlin/com/aryan/calculator/data/UpdateChecker.kt
+++ app/src/main/kotlin/com/aryan/calculator/data/UpdateChecker.kt
@@ -19,7 +19,7 @@ data class AppUpdate(
 object UpdateChecker {
 
     private const val GITHUB_API_URL = "https://api.github.com/repos/chaturvediaryan2024/Swardhun/releases/latest"
-    private const val CURRENT_VERSION_CODE = 31
+    private const val CURRENT_VERSION_CODE = 32
 
     private val client = OkHttpClient()
 
